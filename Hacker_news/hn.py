6b8db336797ebaaf7facd9364bec6f6eeb37d577
Aleksey Strelkov <aleks.strel.8v@gmail.com>
2022-03-03 23:13:32 +0300

    build: доделал компонент парсера
    Текущий функционал выводит название темы и непосредсвтенно ссылку на неё
    при заданном поисковом значении.

--- Hacker_news/hn.py
+++ Hacker_news/hn.py
@@ -12,11 +12,18 @@ soup = BeautifulSoup(response.text, "html.parser")
 # print(soup)
 
 # Находим все теги с нужным классом на странице
-temes = soup.find_all("td", class_="title")
+themes = soup.find_all("td", class_="title")
 
-for teme in temes:  # Циклом идём по всем тегам поочерёдно из temes
-    teme = temes.find("a", {'class': 'titlelink'})  #
-    # print(teme)
-
-    # Создадим условие, где
-
+for topic in themes:  # Циклом идём по всем тегам поочерёдно из temes
+    topic = topic.find("a", {'class': 'titlelink'})
+    # Условимся двигатся дальше без None и зададимся конккретным параметром, который мы ищем (github.com)
+    if topic is not None and 'youtube.com' in str(topic):
+        # Это выдаст список всех тем со страницы
+        # print(topic.text)
+        # Введём переменную, которая уже будет хранить ссылки
+        sublink = topic.get('href')
+        print('===< ~ >===')  # Просто разделитель статей
+        # Склеиваем тему с полученной ссылкой
+        print('\n' + str(topic.text) + " --> " + str(sublink) + '\n')  # .text скрывает ссылку, сохраняя только нужный текст
+else:
+    print('==< ~ >===')  # Так же разделитель для красоты вывода :)
