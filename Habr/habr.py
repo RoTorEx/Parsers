23152773f16482ba24858234a68ce5459db34530
Aleksey Strelkov <aleks.strel.8v@gmail.com>
2022-03-07 12:43:28 +0300

    build: complite 1st page parser
    Рабочий парсер одной страницы поиск по Python и по локации в Минске или
    возможности удалённой работы

--- Habr/habr.py
+++ Habr/habr.py
@@ -2,24 +2,40 @@ from bs4 import BeautifulSoup
 import requests
 
 
+cnt = 0
+
 url = "https://career.habr.com/vacancies?q=python&sort=date&type=all"
 
 response = requests.get(url)
-print(response)
+
 soup = BeautifulSoup(response.text, "html.parser")
 
 themes = soup.find_all('div', class_='vacancy-card__info')
+
+terms_info = ('Можно удаленно', 'Минск')
 
 for theme in themes:  # разбор каждой вакансии на странице по кусочкам
     # Компания
     company = theme.find('div', {'class': "vacancy-card__company-title"}).text
 
     # Тема
-    topic = theme.find('a', {'class': "vacancy-card__title-link"})
+    title = theme.find('a', {'class': "vacancy-card__title-link"})
 
-    # Список скилов
+    # Инфо
+    info = theme.find('div', {'class': "vacancy-card__meta"}).text.split(' • ')
+
+    # зарплата
+    # salary = theme.find('div', {'class': "vacancy-card__salary"}).text.strip()
+
+    # Скилы
     skills = theme.find('div', {'class': "vacancy-card__skills"}).text.split(' • ')
-    if 'Python' in skills and 'удаленно' not in str(topic):
-        sublink = "https://career.habr.com/" + topic.get('href')
-        # print(topic.text, skills)
-        print('==< ~ >==')  # Разделитель
+
+    # Ссылка
+    link = "https://career.habr.com/" + title.get('href')
+
+    if 'удаленно' not in title:
+        if 'Python' in skills and any([item for item in info if item in terms_info]):
+            info = ' '.join(info)
+            skills = ' '.join(skills)
+            print('==< ~ >==')  # Разделитель
+            print(company, title.text, info, skills, link, sep='\n')
